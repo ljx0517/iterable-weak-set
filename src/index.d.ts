2f48c4db172998db9c95a549e151d04a332ddb22
ljx0517 <ljx0517@gmail.com>
2022-03-09 13:06:14 +0800

    feat: add .d.ts

---
+++ src/index.d.ts
@@ -0,0 +1,13 @@
+import { IterableWeakSet } from './index';
+
+/**
+iterable WeakSet.
+
+@example
+```
+import {IterableWeakSet} = require('iterable-weak-set');
+
+const set = new IterableWeakSet();
+```
+*/
+export {IterableWeakSet} from './index'
